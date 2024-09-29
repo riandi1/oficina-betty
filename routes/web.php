3b7a034a4fd83eb311aca30cf01a85ae46fc9436
riandi1 <rendondiaza944@gmail.com>
2024-09-29 18:26:08 -0500

    pagina principal

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $products = [
+        ['id' => 1, 'name' => "Producto 1", 'price' => 10.99, 'category' => "Categoría A", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 2, 'name' => "Producto 2", 'price' => 15.99, 'category' => "Categoría B", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 3, 'name' => "Producto 3", 'price' => 20.99, 'category' => "Categoría A", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 4, 'name' => "Producto 4", 'price' => 25.99, 'category' => "Categoría C", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 1, 'name' => "Producto 1", 'price' => 10.99, 'category' => "Categoría A", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 2, 'name' => "Producto 2", 'price' => 15.99, 'category' => "Categoría B", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 3, 'name' => "Producto 3", 'price' => 20.99, 'category' => "Categoría A", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 4, 'name' => "Producto 4", 'price' => 25.99, 'category' => "Categoría C", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 1, 'name' => "Producto 1", 'price' => 10.99, 'category' => "Categoría A", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 2, 'name' => "Producto 2", 'price' => 15.99, 'category' => "Categoría B", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 3, 'name' => "Producto 3", 'price' => 20.99, 'category' => "Categoría A", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+        ['id' => 4, 'name' => "Producto 4", 'price' => 25.99, 'category' => "Categoría C", 'image' => "BOM-BOM-BUM-1-removebg-preview.png"],
+    ];
+    return view('welcome',compact('products'));
 });
 
 Route::middleware([
